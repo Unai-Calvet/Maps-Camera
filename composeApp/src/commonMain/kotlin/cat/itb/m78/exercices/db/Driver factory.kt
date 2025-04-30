4acd0b853bfc750b12cb44a8d403364bf97a3e26
unai.calvet.7e8 <unai.calvet.7e8@itb.cat>
2025-04-30 13:05:47 +0200

    _

---
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/db/Driver factory.kt
@@ -0,0 +1,10 @@
+package cat.itb.m78.exercices.db
+
+import app.cash.sqldelight.db.SqlDriver
+
+expect fun createDriver(): SqlDriver
+fun createDatabase(): Database {
+    val driver = createDriver()
+    return Database(driver)
+}
+val database by lazy { createDatabase() }
