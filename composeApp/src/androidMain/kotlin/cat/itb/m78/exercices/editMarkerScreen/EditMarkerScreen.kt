d61913d4682db829a19ee80b034c185f1d19cbeb
Unai Calvet <unai.calvet.7e8@itb.cat>
2025-06-11 19:08:43 +0200

    Slecció d'imatges afegida

--- composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/editMarkerScreen/EditMarkerScreen.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/editMarkerScreen/EditMarkerScreen.kt
@@ -1,9 +1,14 @@
 package cat.itb.m78.exercices.editMarkerScreen
 
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -14,8 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cat.itb.m78.exercices.db.Marker
+import coil3.compose.AsyncImage
 
 @Composable
 fun EditMarkerScreen(navigateToListScreen: () -> Unit, id : Long, navigateToCameraScreen : (Long) -> Unit) {
@@ -34,6 +41,15 @@ fun EditMarkerScreen(
     val imageUri = remember { mutableStateOf(marker.imageUri) }
     val info = remember { mutableStateOf(marker.info) }
     val description = remember { mutableStateOf(marker.description) }
+
+    val pickMedia = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        if (uri != null) {
+            Log.d("PhotoPicker", "Selected URI: $uri")
+            imageUri.value = uri.toString()
+        } else {
+            Log.d("PhotoPicker", "No media selected")
+        }
+    }
 
     if (title.value == null) {
         title.value = ""
@@ -61,8 +77,20 @@ fun EditMarkerScreen(
 
         Spacer(modifier = Modifier.size(75.dp))
 
+        if (imageUri != null) {
+            AsyncImage(
+                model = marker.imageUri?.toUri(),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth().size(250.dp)
+            )
+        }
+
         Button(onClick = {onClickPhoto(title.value!!, imageUri.value!!, info.value!!, description.value!!, marker.id, navigateToCameraScreen)}) {
             Text("Fer Foto")
+        }
+
+        Button(onClick = {pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))}) {
+            Text("Selecionar foto")
         }
 
         Spacer(modifier = Modifier.size(75.dp))
